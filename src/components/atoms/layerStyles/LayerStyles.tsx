7d8a480ba75c4d4cb54f95fa55065ae0dab2ad41
„tassilomorino“ <tassilomorino@gmail.com>
2022-04-21 10:43:34 +0200

    add first layer styles

---
+++ src/components/atoms/layerStyles/LayerStyles.tsx
@@ -0,0 +1,12 @@
+/** @format */
+import styled, { css } from "styled-components";
+
+export const LayerWhitFirstDefault = css`
+  box-sizing: border-box;
+  box-shadow: 0px 12px 18px -8px rgba(186, 160, 79, 0.2) 0px -4px 10px 4px
+    rgba(255, 255, 255, 0.2);
+  background-color: #fcfbf8;
+  overflow: visible;
+  border-radius: 18px;
+  border: 2px solid #ffffff;
+`;
