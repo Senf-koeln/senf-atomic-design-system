07d483f900e30d4a730ff3793b600da713685092
„tassilomorino“ <tassilomorino@gmail.com>
2022-04-21 09:13:26 +0200

    implement Storybook Themeprovider

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,22 @@
 /** @format */
 
+import { ThemeProvider } from "styled-components";
+import { addDecorator } from "@storybook/react";
+import { withThemes } from "@react-theming/storybook-addon";
+
+import { theme } from "../src/theme/theme";
+
 module.exports = {
   stories: [
     "../src/**/**/*.stories.mdx",
     "../src/**/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@react-theming/storybook-addon",
+  ],
 };
+
+// pass ThemeProvider and array of your themes to decorator
+addDecorator(withThemes(ThemeProvider, [theme]));
