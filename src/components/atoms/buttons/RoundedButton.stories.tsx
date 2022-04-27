96cbca0bd8c0453ac635a4dc623dfa9dd4905a8d
„tassilomorino“ <tassilomorino@gmail.com>
2022-04-27 09:12:17 +0200

    split up roundedbutton stories

---
+++ src/components/atoms/buttons/RoundedButton.stories.tsx
@@ -0,0 +1,26 @@
+/** @format */
+
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+
+import RoundedButton from "./RoundedButton";
+
+import { ButtonProps } from "./Button.types";
+
+export default {
+  title: "Atom/RoundedButton",
+  component: RoundedButton,
+  argTypes: {},
+} as Meta<typeof RoundedButton>;
+
+const Template: Story<ButtonProps> = (args) => <RoundedButton {...args} />;
+
+export const AddButton = Template.bind({});
+AddButton.args = {
+  variant: "plus",
+};
+
+export const BackButton = Template.bind({});
+BackButton.args = {
+  icon: "arrowdown",
+};
