156b3fd4fd69a4f1888143e59755ff612cf04dd3
„tassilomorino“ <tassilomorino@gmail.com>
2022-05-02 08:49:14 +0200

    enhance flexwrapper and layerstyles

--- src/components/atoms/layout/FlexWrapper.tsx
+++ src/components/atoms/layout/FlexWrapper.tsx
@@ -6,11 +6,15 @@ import { FlexWrapperProps } from "./FlexWrapper.types";
 
 const Wrapper = styled.div<FlexWrapperProps>`
   width: ${(props) => (props.width ? props.width : "100%")};
+  width: ${(props) => (props.height ? props.height : "auto")};
+
   margin: ${(props) => (props.margin ? props.margin : "0")};
   display: flex;
   flex-direction: ${(props) =>
     props.direction === "vertical" ? "column" : "row"};
   align-items: ${(props) => (props.alignItems ? props.alignItems : null)};
+  justify-content: ${(props) =>
+    props.justifyContent ? props.justifyContent : null};
   gap: ${(props) => (props.gap ? props.gap : null)};
 `;
 
@@ -21,6 +25,8 @@ const FlexWrapper: FC<FlexWrapperProps> = ({
   margin,
   width,
   alignItems,
+  justifyContent,
+  height,
 }) => {
   return (
     <Wrapper
@@ -28,7 +34,9 @@ const FlexWrapper: FC<FlexWrapperProps> = ({
       gap={gap}
       margin={margin}
       width={width}
+      height={height}
       alignItems={alignItems}
+      justifyContent={justifyContent}
     >
       {children}
     </Wrapper>
