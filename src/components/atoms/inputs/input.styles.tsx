97f05c5a351554751eefe220c7dbc46708d7feed
Enjoy2Live <iMohammedMaqbol@gmail.com>
2022-05-05 13:33:19 +0300

    add label and note variants

--- src/components/atoms/inputs/input.styles.tsx
+++ src/components/atoms/inputs/input.styles.tsx
@@ -2,13 +2,10 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: inline-flex;
-  ::placeholder,
-  ::-webkit-input-placeholder {
-    color: rgba(0, 0, 0, 0.7);
-  }
-  :-ms-input-placeholder {
-    color: rgba(0, 0, 0, 0.7);
-  }
+  flex-direction: column;
+  gap: 0.2rem;
+  color: ${({theme}) => theme.colors.black.black40tra};
+  max-width: 13rem;
   &:focus {
     outline: 3px solid ${(props) => props.theme.colors.primary.primary120};
     outline-offset: -3px;
@@ -17,6 +14,18 @@ export const Wrapper = styled.div`
 
 export const Indication = styled.div`
   display: inline-flex;
+  justify-content: space-between;
+  label {
+    font-size: ${({theme}) => theme.fontSizes[1]}rem;
+    font-weight: ${({theme}) => theme.fontWeights[1]};
+    line-height: ${({theme}) => theme.lineHeight[0]};
+  }
+
+  p {
+    font-size: ${({theme}) => theme.fontSizes[0]}rem;
+    line-height: ${({theme}) => theme.lineHeight[2]};
+    flex-basis: 7rem;
+  }
 `;
 
 export const InputField = styled.div`
@@ -29,13 +38,13 @@ export const TextField = styled.input`
   padding: 0.5rem 1rem;
 
   background-color: rgba(255, 255, 255, 0.5);
-
+  color: rgb(51, 51, 51);
   ::placeholder,
   ::-webkit-input-placeholder {
-    color: rgba(0, 0, 0, 0.4);
+    color: ${({theme}) => theme.colors.black.black40tra};
   }
   :-ms-input-placeholder {
-    color: rgba(0, 0, 0, 0.4);
+    color: ${({theme}) => theme.colors.black.black40tra};
   }
 
   &:focus {
