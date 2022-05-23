8aa0f231e9039fd3c0209a431f20c921631f7b42
Enjoy2Live <iMohammedMaqbol@gmail.com>
2022-05-23 15:55:27 +0300

    fix(Enjoy2Live): fix-typography-typing

--- src/components/atoms/typography/Typography.types.ts
+++ src/components/atoms/typography/Typography.types.ts
@@ -1,5 +1,5 @@
 export interface TypographyProps {
-    fontFamily: string;
+    fontFamily?: string;
     variant: "h1" | "h2" | "h3" | "buttonBg" | "buttonSm" | "bodyBg" | "bodySm" | "footnote";
 
 }
