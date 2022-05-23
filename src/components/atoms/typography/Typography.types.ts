77c1eea229637e963ece6e642e2b9fded7e268f1
Enjoy2Live <iMohammedMaqbol@gmail.com>
2022-05-23 16:13:51 +0300

    feat(Enjoy2Live): add color, textAlign, fontSize props

--- src/components/atoms/typography/Typography.types.ts
+++ src/components/atoms/typography/Typography.types.ts
@@ -1,5 +1,7 @@
 export interface TypographyProps {
     fontFamily: string;
+    fontSize?: string;
+    color?: string;
+    textAlign?: string;
     variant: "h1" | "h2" | "h3" | "buttonBg" | "buttonSm" | "bodyBg" | "bodySm" | "footnote";
-
 }
